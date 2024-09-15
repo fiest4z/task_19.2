374f3eb59a6381f22c33b9cbe43bd317803ca91a
Mikhail Belyaev <fiestaz1337@gmail.com>
2024-09-15 20:59:17 +0300

    fin 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import NewappConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_details
 
 app_name = NewappConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('catalog/<int:pk>', product_details, name='product_details')
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    product_list = Product.objects.all()
+    context = {"product_list": product_list}
+    return render(request, 'product_list.html', context)
 
 
 def contacts(request):
@@ -14,3 +18,8 @@ def contacts(request):
         message = request.POST['message']
         print(f'{name} {phone} {message}')
     return render(request, 'contacts.html')
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_details.html', context)
